bb64efe59aa95666b5efd2fad402904d3e026a65
prabhu-sk1701 <prabhu.sk1701@gmail.com>
2024-10-12 22:22:35 +0530

    Final functionality added

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,7 +4,7 @@ import random
 
 window = Tk()
 window.title('Rock Paper and Scissors')
-window.geometry("400x400")
+window.geometry("300x300")
 
 computer_moves = {
     '0' : 'Rock',
@@ -12,6 +12,59 @@ computer_moves = {
     '2' : 'Scissor'
 }
 
+def reset():
+    b1['state'] = 'active'
+    b2['state'] = 'active'
+    b3['state'] = 'active'
+    l1.config(text='Player          ')
+    l3.config(text='Computer')
+    l4.config(text='')
+    
+def disable_buttons():
+    b1['state'] = 'disable'
+    b2['state'] = 'disable'
+    b3['state'] = 'disable'
+    
+def isrock():
+    move = computer_moves[str(random.randint(0,2))]
+    if move == 'Rock':
+        result = 'Draw'
+    elif move == 'Scissor':
+        result = 'Player Wins'
+    else:
+        result = 'Computer Wins'
+    l4.config(text=result)
+    l1.config(text='Rock            ')
+    l3.config(text=move)
+    disable_buttons()
+    
+def ispaper():
+    move = computer_moves[str(random.randint(0,2))]
+    if move == 'Paper':
+        result = 'Draw'
+    elif move == 'Rock':
+        result = 'Player Wins'
+    else:
+        result = 'Computer Wins'
+    l4.config(text=result)
+    l1.config(text='Paper            ')
+    l3.config(text=move)
+    disable_buttons()
+    
+def isscissor():
+    move = computer_moves[str(random.randint(0,2))]
+    if move == 'Scissor':
+        result = 'Draw'
+    elif move == 'Paper':
+        result = 'Player Wins'
+    else:
+        result = 'Computer Wins'
+    l4.config(text=result)
+    l1.config(text='Scissor            ')
+    l3.config(text=move)
+    disable_buttons()
+
+#GUI setup
 Label(window,
       text='Rock Paper Scissors',
       font='normal 20 bold',
@@ -46,13 +99,13 @@ frame1.pack()
 
 b1 = Button(frame1, text='Rock',
             font=10, width=7,
-            command='''isrock''')
-b2 = Button(frame1, text='Rock',
+            command=isrock)
+b2 = Button(frame1, text='Paper',
             font=10, width=7,
-            command='''isrock''')
+            command=ispaper)
 b3 = Button(frame1, text='Rock',
             font=10, width=7,
-            command='''isrock''')
+            command=isscissor)
 
 b1.pack(side=LEFT, padx=10)
 b2.pack(side=LEFT, padx=10)
@@ -60,6 +113,6 @@ b3.pack(padx=10)
 
 Button(window, text='Reset',
        font=10, fg='red',
-       bg='black', command='''reset_game''').pack(pady=20)
+       bg='black', command=reset).pack(pady=20)
 
 window.mainloop()
